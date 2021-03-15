2dca59a2077599f10e895d1d51aec7b6ed2e01ad
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2021-03-15 22:44:00 +0300

    Add build folder

---
+++ precache-manifest.146072f83ec7a16c7b98df6cbffca22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dc203f10d1d2400ef4805ab6ee654f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8341d63202c6d9474e07",
+    "url": "/static/css/main.2bacda3b.chunk.css"
+  },
+  {
+    "revision": "2b655a99bc61075cae4f",
+    "url": "/static/js/2.7f973d3f.chunk.js"
+  },
+  {
+    "revision": "8341d63202c6d9474e07",
+    "url": "/static/js/main.e42d4eb7.chunk.js"
+  },
+  {
+    "revision": "fd9328d9c5d5a6902af9",
+    "url": "/static/js/runtime~main.0bff5dd2.js"
+  },
+  {
+    "revision": "3e5aaa0b12bdea66a76afa80b1cb57f4",
+    "url": "/static/media/myprofile.3e5aaa0b.jpg"
+  }
+]);
